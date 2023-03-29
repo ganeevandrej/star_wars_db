9d2748e96029e64c34727af16e2caae77ca31737
andrei <ganeevandrej21@gmail.com>
2023-03-29 19:27:24 +0300

    Получение данных c Api и дообавление методов ,отвечающих за трансформацию данных, в класс SwapiService.

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,27 +1,51 @@
 import React, { Component } from 'react';
 
+import { SwapiService } from "../../services/swapi-service";
+
 import './random-planet.css';
 
 export class RandomPlanet extends Component {
 
+    constructor() {
+        super();
+        this.updatePlanet();
+    }
+
+    swapiService = new SwapiService();
+
+    state = {
+        planet: {}
+    }
+
+    onPlanetLoaded = (planet) => this.setState(planet);
+
+    updatePlanet() {
+        const id = Math.ceil(Math.random() * 18) + 1;
+
+        this.swapiService.getPlanet(id)
+            .then(this.onPlanetLoaded)
+    }
+
     render() {
+        const { planet: { id, name, population, rotationPeriod, diameter } } = this.state;
+
         return (
             <div className="random-planet jumbotron rounded">
                 <img className="planet-image"
-                     src="https://starwars-visualguide.com/assets/img/planets/5.jpg" alt="" />
+                     src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt="" />
                 <div>
-                    <h4>Planet Name</h4>
+                    <h4>{ name }</h4>
                     <ul className="list-group list-group-flush">
                         <li className="list-group-item">
-                            <span className="term">Population</span>
+                            <span className="term">{ population }</span>
                             <span>123124</span>
                         </li>
                         <li className="list-group-item">
-                            <span className="term">Rotation Period</span>
+                            <span className="term">{ rotationPeriod }</span>
                             <span>43</span>
                         </li>
                         <li className="list-group-item">
-                            <span className="term">Diameter</span>
+                            <span className="term">{ diameter }</span>
                             <span>100</span>
                         </li>
                     </ul>
--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -7,8 +7,6 @@ export class SwapiService {
     async getResource(url) {
         const res = await axios(`${this._apiBase}${url}`);
 
-        console.log(res);
-
         if (res.status !== 200) {
             throw new Error(`Could not fetch ${url}` +
                 `, received ${res.status}`)
@@ -19,29 +17,70 @@ export class SwapiService {
 
     getAllPeople = async () => {
         const res = await this.getResource(`/people/`);
-        return res.results;
+        return res.results.map(this._transformPerson);
     }
 
-    getPerson = (id) => {
-        return this.getResource(`/people/${id}/`);
+    getPerson = async (id) => {
+        const res = await this.getResource(`/people/${id}/`);
+        return this._transformPerson(res);
     }
 
      getAllPlanets = async () => {
         const res = await this.getResource(`/planets/`);
-        return res.results;
+        return res.results.map(this._transformPlanet);
     }
 
-    getPlanet = (id) => {
-        return this.getResource(`/planets/${id}/`);
+    getPlanet = async (id) => {
+        const res = await this.getResource(`/planets/${id}/`);
+        return this._transformPlanet(res);
     }
 
     getAllStarships = async () => {
         const res = await this.getResource(`/starships/`);
-        return res.results;
+        return res.results.map(this._transformStarship);
     }
 
-    getStarship = (id) => {
-        return this.getResource(`/starships/${id}/`);
+    getStarship = async (id) => {
+        const res = await this.getResource(`/starships/${id}/`);
+        return this._transformStarship(res);
+    }
+
+    _extractId(url) {
+        const idRegExp = /\/([0-9]*)\/$/;
+        return url.match(idRegExp)[1];
+    }
+
+    _transformPlanet = (planet) => {
+        return {
+            id: this._extractId(planet.url),
+            name: planet.name,
+            population: planet.population,
+            rotationPeriod: planet.rotation_period,
+            diameter: planet.diameter
+        }
+    }
+
+    _transformStarship = (starship) => {
+        return {
+            id: this._extractId(starship),
+            name: starship.name,
+            model: starship.model,
+            manufacturer: starship.manufacturer,
+            costInCredits: starship.costInCredits,
+            length: starship.length,
+            crew: starship.crew,
+            passengers: starship.passengers,
+            cargoCapacity: starship.cargoCapacity
+        }
+    }
+
+    _transformPerson = (person) => {
+        return {
+            id: this._extractId(person),
+            name: person.name,
+            gender: person.gender,
+            birthYear: person.birthYear,
+            eyeColor: person.eyeColor
+        }
     }
 }
-
